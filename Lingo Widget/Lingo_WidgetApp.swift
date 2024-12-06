f36b0fe5f8802d045b0cf5c47e48f406875cfa51
Oguz Doruk <oguzkaratoruk@yandex.com>
2024-12-06 13:39:24 +0800

    Initial Commit

---
+++ Lingo Widget/Lingo_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lingo_WidgetApp.swift
+//  Lingo Widget
+//
+//  Created by Oguz Doruk on 6.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Lingo_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
