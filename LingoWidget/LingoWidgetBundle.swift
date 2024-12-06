db21f2394b35f3a439c686669c8a7b3c73ac80a3
Oguz Doruk <oguzkaratoruk@yandex.com>
2024-12-06 13:44:44 +0800

    Project Created, gitignore

---
+++ LingoWidget/LingoWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  LingoWidgetBundle.swift
+//  LingoWidget
+//
+//  Created by Oguz Doruk on 6.12.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LingoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LingoWidget()
+        LingoWidgetControl()
+    }
+}
